97647e72a32ceef54881d4648c976aaecf9e5168
Ersin Çöl <ersincol235@gmail.com>
2023-10-29 13:13:01 +0300

    create library store, addBookToLibrary function, book constructor function, reset form function

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const dialog = document.querySelector(".modal");
+const modal = document.querySelector(".modal");
 const newBookBtn = document.querySelector(".new-book-btn");
 const closeBtn = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(".submit-btn");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const numberOfPages = document.querySelector("#page");
+const isRead = document.querySelector("#is_read");
+
+const library = [];
+
+function Book(author, title, numberOfPages, isRead){
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(book){
+    library.push(book);
+}
+
+function resetForm(){
+    author.value = "";
+    title.value = "";
+    numberOfPages.value = "";
+    isRead.checked = false;
+}
 
 newBookBtn.addEventListener("click", ()=>{
-    dialog.showModal();
+    modal.showModal();
 })
 
 closeBtn.addEventListener("click", ()=>{
-    dialog.close();
+    modal.close();
+})
+
+submitBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const book = new Book( author.value, title.value, numberOfPages.value, isRead.checked );
+    addBookToLibrary(book);
+    resetForm();
+    modal.close();
 })
