cf257d866c4b4b23ddaceaba1174876635b341d8
Ersin Çöl <ersincol235@gmail.com>
2023-11-27 13:28:20 +0300

    refactor: create book, library and modal class. create screen controller and apply update screen pattern

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const modal = document.querySelector(".modal");
-const newBookBtn = document.querySelector(".new-book-btn");
-const closeBtn = document.querySelector(".close-btn");
-const submitBtn = document.querySelector(".submit-btn");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const publisher = document.querySelector("#publisher");
-const numberOfPages = document.querySelector("#page");
-const isRead = document.querySelector("#is_read");
-const tableEl = document.querySelector("table");
-const TBodyEl = document.querySelector("tbody");
-const THeadEl = document.querySelector("thead");
-const noContentEl = document.querySelector(".no-content");
 
-const library = [];
+class Modal {
+    static open(){
+        modal.showModal();
+    }
+
+    static close(){
+        modal.close();
+    }
+}
+
+class Library {
+    #books = [];
+
+    getBooks() {
+        return this.#books;
+    }
+
+    addBook(book) {
+        this.#books.push(book);
+    }
+
+    removeBook(index){
+        if(index >= 0 && index < this.#books.length){
+            this.#books.splice(index, 1);
+        }
+    }
+
+    bookCount = () => this.#books.length;
+}
 
 class Book {
     constructor(author, title, publisher, numberOfPages, isRead) {
@@ -28,125 +44,142 @@ class Book {
     }
 }
 
-function addBookToLibrary(book){
-    library.push(book);
-    renderBook(book, library.length);
-    noContentEl.classList.add("hidden");
-    tableEl.classList.remove("hidden");
-}
-function resetForm(){
-    author.value = "";
-    title.value = "";
-    publisher.value = "";
-    numberOfPages.value = "";
-    isRead.checked = false;
-}
-function openModal(){
-    modal.showModal();
-}
-function closeModal(){
-    resetForm();
-    modal.close();
-}
+const ScreenController = function (){
+    const newBookBtn = document.querySelector(".new-book-btn");
+    const closeBtn = document.querySelector(".close-btn");
+    const submitBtn = document.querySelector(".submit-btn");
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const publisher = document.querySelector("#publisher");
+    const numberOfPages = document.querySelector("#page");
+    const isRead = document.querySelector("#is_read");
+    const tableEl = document.querySelector("table");
+    const TBodyEl = document.querySelector("tbody");
+    const THeadEl = document.querySelector("thead");
+    const noContentEl = document.querySelector(".no-content");
 
-function handleAddBook(event){
-    event.preventDefault();
-    if(library.length === 0 && !document.querySelector("thead > tr")){
-        createTableHeader();
-    }
-    const book = new Book( author.value, title.value, publisher.value, numberOfPages.value, isRead.checked ? "yes" : "no" );
-    addBookToLibrary(book);
-    closeModal();
-}
-function renderBook(book, index){
-    const row = document.createElement("tr");
-    for (const key in book) {
-        if(book.hasOwnProperty(key)){
-            const cell = document.createElement("td");
-            if(key === "isRead"){
-                const toggleStatusBtn = document.createElement("button");
-                toggleStatusBtn.className = "toggle-status-btn";
-                toggleStatusBtn.dataset.indexNumber = `${index}`;
-                toggleStatusBtn.innerText = book[key];
-                if(book[key] === "yes"){
-                    toggleStatusBtn.classList.add("read-status");
-                }
-                toggleStatusBtn.addEventListener("click", ()=>{
-                    book.toggleReadStatus();
-                    toggleStatusBtn.innerText = book.isRead;
-                    toggleStatusBtn.classList.toggle("read-status", book.isRead === "yes");
-                })
-                cell.className = "status-cell";
-                cell.appendChild(toggleStatusBtn);
-            }else{
-                cell.innerText = book[key];
+    const library = new Library();
+    const updateScreen = () => {
+        THeadEl.innerHTML = "";
+        TBodyEl.innerHTML = "";
+
+        if(library.bookCount() > 0) {
+            noContentEl.classList.add("hidden");
+            if(!document.querySelector("thead > tr")){
+                createTableHeader();
             }
-            row.appendChild(cell);
-        }
-    }
-    const actionCell = document.createElement("td");
-    const removeBtn = document.createElement("button");
-    removeBtn.className = "delete-btn";
-    removeBtn.dataset.indexNumber = `${index}`;
-    removeBtn.addEventListener("click", (event)=>{
-        const selectedBookIndex = event.target.dataset.indexNumber;
-        library.splice(parseInt(selectedBookIndex), 1);
-        TBodyEl.removeChild(row);
-        if(library.length === 0){
+            library.getBooks().forEach((book, index)=> {
+                const row = document.createElement("tr");
+                for (const key in book) {
+                    if(book.hasOwnProperty(key)){
+                        const cell = document.createElement("td");
+                        if(key === "isRead"){
+                            const toggleStatusBtn = document.createElement("button");
+                            toggleStatusBtn.className = "toggle-status-btn";
+                            toggleStatusBtn.dataset.indexNumber = `${index}`;
+                            toggleStatusBtn.innerText = book[key];
+                            if(book[key] === "yes"){
+                                toggleStatusBtn.classList.add("read-status");
+                            }
+                            toggleStatusBtn.addEventListener("click", ()=>{
+                                book.toggleReadStatus();
+                                toggleStatusBtn.innerText = book.isRead;
+                                toggleStatusBtn.classList.toggle("read-status", book.isRead === "yes");
+                            })
+                            cell.className = "status-cell";
+                            cell.appendChild(toggleStatusBtn);
+                        }else{
+                            cell.innerText = book[key];
+                        }
+                        row.appendChild(cell);
+                    }
+                }
+                const actionCell = document.createElement("td");
+                const removeBtn = document.createElement("button");
+                removeBtn.className = "delete-btn";
+                removeBtn.dataset.indexNumber = `${index}`;
+                removeBtn.addEventListener("click", (event)=>{
+                    const selectedBookIndex = event.currentTarget.dataset.indexNumber;
+                    library.removeBook(parseInt(selectedBookIndex))
+                    updateScreen();
+                })
+                const removeIcon = document.createElement("img");
+                removeIcon.src = "assets/delete-outline.svg";
+                removeIcon.alt = "delete-icon";
+                removeIcon.className = "delete-icon";
+
+                removeBtn.appendChild(removeIcon);
+                actionCell.appendChild(removeBtn);
+                row.appendChild(actionCell);
+
+                TBodyEl.appendChild(row);
+            });
+            tableEl.classList.remove("hidden");
+        }else {
             tableEl.classList.add("hidden");
             noContentEl.classList.remove("hidden");
         }
-    })
-    const removeIcon = document.createElement("img");
-    removeIcon.src = "assets/delete-outline.svg";
-    removeIcon.alt = "delete-icon";
-    removeIcon.className = "delete-icon";
-
-    removeBtn.appendChild(removeIcon);
-    actionCell.appendChild(removeBtn);
-    row.appendChild(actionCell);
-
-    TBodyEl.appendChild(row);
-}
-function createTableHeader(){
-    const tableHeaderRow = document.createElement("tr");
-    const authorCol = document.createElement("th");
-    authorCol.innerText = "Author";
-    const titleCol = document.createElement("th");
-    titleCol.innerText = "Title";
-    const publisherCol = document.createElement("th");
-    publisherCol.innerText = "Publisher";
-    const pageCol = document.createElement("th");
-    pageCol.innerText = "Page"
-    const isReadCol = document.createElement("th")
-    isReadCol.innerText = "Is Read"
-    const actionCol = document.createElement("th");
-
-    tableHeaderRow.appendChild(authorCol)
-    tableHeaderRow.appendChild(titleCol)
-    tableHeaderRow.appendChild(publisherCol)
-    tableHeaderRow.appendChild(pageCol)
-    tableHeaderRow.appendChild(isReadCol)
-    tableHeaderRow.appendChild(actionCol)
-
-    THeadEl.appendChild(tableHeaderRow)
-}
-
-newBookBtn.addEventListener("click", openModal)
-
-closeBtn.addEventListener("click", closeModal)
-
-submitBtn.addEventListener("click", handleAddBook)
-
-document.addEventListener("DOMContentLoaded", function (){
-    if (library.length && !document.querySelector("thead > tr")){
-        noContentEl.classList.add("hidden");
-        createTableHeader();
-        library.forEach(renderBook);
-    }else{
-        tableEl.classList.add("hidden");
     }
-})
 
+    const createTableHeader = () => {
+        const tableHeaderRow = document.createElement("tr");
+        const authorCol = document.createElement("th");
+        authorCol.innerText = "Author";
+        const titleCol = document.createElement("th");
+        titleCol.innerText = "Title";
+        const publisherCol = document.createElement("th");
+        publisherCol.innerText = "Publisher";
+        const pageCol = document.createElement("th");
+        pageCol.innerText = "Page"
+        const isReadCol = document.createElement("th")
+        isReadCol.innerText = "Is Read"
+        const actionCol = document.createElement("th");
 
+        tableHeaderRow.appendChild(authorCol)
+        tableHeaderRow.appendChild(titleCol)
+        tableHeaderRow.appendChild(publisherCol)
+        tableHeaderRow.appendChild(pageCol)
+        tableHeaderRow.appendChild(isReadCol)
+        tableHeaderRow.appendChild(actionCol)
 
+        THeadEl.appendChild(tableHeaderRow)
+    }
+
+    function resetForm(){
+        author.value = "";
+        title.value = "";
+        publisher.value = "";
+        numberOfPages.value = "";
+        isRead.checked = false;
+    }
+
+    newBookBtn.addEventListener("click", () => {
+        Modal.open();
+    })
+
+    closeBtn.addEventListener("click", () => {
+        resetForm();
+        Modal.close();
+    })
+
+    submitBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        if(library.bookCount() === 0 && !document.querySelector("thead > tr")){
+            createTableHeader();
+        }
+        const book = new Book( author.value, title.value, publisher.value, numberOfPages.value, isRead.checked ? "yes" : "no" );
+        library.addBook(book);
+        resetForm();
+        Modal.close();
+        updateScreen();
+    })
+
+    // initial render
+    updateScreen();
+
+    return {
+        updateScreen,
+    }
+}
+
+ScreenController();
