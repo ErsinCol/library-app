9e4ba0ec345908e82afb62402fec82daa5331ab6
Ersin Çöl <ersincol235@gmail.com>
2023-11-27 10:40:16 +0300

    refactor: code to use classes

--- index.js
+++ index.js
@@ -13,163 +13,24 @@ const THeadEl = document.querySelector("thead");
 const noContentEl = document.querySelector(".no-content");
 
 const library = [];
-const mockData = [
-        {
-            author: "J.K. Rowling",
-            title: "Harry Potter and the Sorcerer's Stone",
-            publisher: "Scholastic",
-            numberOfPages: 320,
-            isRead: "yes"
-        },
-        {
-            author: "George Orwell",
-            title: "1984",
-            publisher: "Penguin Books",
-            numberOfPages: 328,
-            isRead: "yes"
-        },
-        {
-            author: "Harper Lee",
-            title: "To Kill a Mockingbird",
-            publisher: "Harper Perennial",
-            numberOfPages: 281,
-            isRead: "yes"
-        },
-        {
-            author: "J.R.R. Tolkien",
-            title: "The Lord of the Rings: The Fellowship of the Ring",
-            publisher: "Mariner Books",
-            numberOfPages: 432,
-            isRead: "no"
-        },
-        {
-            author: "Dan Brown",
-            title: "The Da Vinci Code",
-            publisher: "Anchor Books",
-            numberOfPages: 454,
-            isRead: "no"
-        },
-        {
-            author: "Agatha Christie",
-            title: "Murder on the Orient Express",
-            publisher: "William Morrow Paperbacks",
-            numberOfPages: 256,
-            isRead: "yes"
-        },
-        {
-            author: "Jane Austen",
-            title: "Pride and Prejudice",
-            publisher: "Penguin Classics",
-            numberOfPages: 432,
-            isRead: "no"
-        },
-        {
-            author: "F. Scott Fitzgerald",
-            title: "The Great Gatsby",
-            publisher: "Scribner",
-            numberOfPages: 180,
-            isRead: "yes"
-        },
-        {
-            author: "Mark Twain",
-            title: "The Adventures of Huckleberry Finn",
-            publisher: "Dover Publications",
-            numberOfPages: 224,
-            isRead: "no"
-        },
-        {
-            author: "Leo Tolstoy",
-            title: "War and Peace",
-            publisher: "Vintage Classics",
-            numberOfPages: 1392,
-            isRead: "no"
-        },
-        {
-            author: "Stephen King",
-            title: "It",
-            publisher: "Scribner",
-            numberOfPages: 1138,
-            isRead: "no"
-        },
-        {
-            author: "Herman Melville",
-            title: "Moby-Dick",
-            publisher: "Penguin Classics",
-            numberOfPages: 720,
-            isRead: "no"
-        },
-        {
-            author: "Margaret Atwood",
-            title: "The Handmaid's Tale",
-            publisher: "Anchor Books",
-            numberOfPages: 311,
-            isRead: "yes"
-        },
-        {
-            author: "Ernest Hemingway",
-            title: "The Old Man and the Sea",
-            publisher: "Scribner",
-            numberOfPages: 127,
-            isRead: "yes"
-        },
-        {
-            author: "Jules Verne",
-            title: "Twenty Thousand Leagues Under the Sea",
-            publisher: "Puffin Classics",
-            numberOfPages: 352,
-            isRead: "yes"
-        },
-        {
-            author: "Ray Bradbury",
-            title: "Fahrenheit 451",
-            publisher: "Simon & Schuster",
-            numberOfPages: 249,
-            isRead: "yes"
-        },
-        {
-            author: "Emily Brontë",
-            title: "Wuthering Heights",
-            publisher: "Penguin Classics",
-            numberOfPages: 416,
-            isRead: "yes"
-        },
-        {
-            author: "Arthur Conan Doyle",
-            title: "Sherlock Holmes: The Hound of the Baskervilles",
-            publisher: "Penguin Classics",
-            numberOfPages: 256,
-            isRead: "yes"
-        },
-        {
-            author: "Aldous Huxley",
-            title: "Brave New World",
-            publisher: "Harper Perennial",
-            numberOfPages: 288,
-            isRead: "no"
-        },
-        {
-            author: "Agatha Christie",
-            title: "And Then There Were None",
-            publisher: "William Morrow Paperbacks",
-            numberOfPages: 264,
-            isRead: "no"
-        }
-]
 
-function Book(author, title, publisher, numberOfPages, isRead){
-    this.author = author;
-    this.title = title;
-    this.publisher = publisher;
-    this.numberOfPages = numberOfPages;
-    this.isRead = isRead;
-}
-Book.prototype.toggleReadStatus = function (){
-    this.isRead = this.isRead === "yes" ? "no" : "yes";
+class Book {
+    constructor(author, title, publisher, numberOfPages, isRead) {
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+        this.numberOfPages = numberOfPages;
+        this.isRead = isRead;
+    }
+
+    toggleReadStatus() {
+        this.isRead = this.isRead === "yes" ? "no" : "yes";
+    }
 }
 
 function addBookToLibrary(book){
     library.push(book);
-    renderBook(book);
+    renderBook(book, library.length);
     noContentEl.classList.add("hidden");
     tableEl.classList.remove("hidden");
 }
@@ -187,6 +48,7 @@ function closeModal(){
     resetForm();
     modal.close();
 }
+
 function handleAddBook(event){
     event.preventDefault();
     if(library.length === 0 && !document.querySelector("thead > tr")){
@@ -276,23 +138,7 @@ closeBtn.addEventListener("click", closeModal)
 
 submitBtn.addEventListener("click", handleAddBook)
 
-function fillDataTable(){
-    mockData.forEach((book)=>{
-        const bookObj = new Book(
-            book.author,
-            book.title,
-            book.publisher,
-            book.numberOfPages,
-            book.isRead,
-        )
-
-        library.push(bookObj)
-    })
-}
-
 document.addEventListener("DOMContentLoaded", function (){
-    fillDataTable();
-
     if (library.length && !document.querySelector("thead > tr")){
         noContentEl.classList.add("hidden");
         createTableHeader();
